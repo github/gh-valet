49cec524c09793b0862016886453835fa65e8053
Ethan Dennis <erdennis13@gmail.com>
2022-06-07 14:00:18 -0700

    Document the configure command

--- src/Valet/Commands/Configure.cs
+++ src/Valet/Commands/Configure.cs
@@ -6,7 +6,7 @@ namespace Valet.Commands;
 public class Configure : BaseCommand
 {
     protected override string Name => "configure";
-    protected override string Description => "Start an interactive prompt to configure credentials used to authenticate to your CI server(s).";
+    protected override string Description => "Start an interactive prompt to configure credentials used to authenticate with your CI server(s).";
 
     protected override Command GenerateCommand(App app)
     {
