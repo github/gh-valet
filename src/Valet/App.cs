20fa25e48f7407cd391aa28f50de5f34204a55cf
Ethan Dennis <erdennis13@gmail.com>
2022-06-08 15:14:07 -0700

    Format all version information with the version command

--- src/Valet/App.cs
+++ src/Valet/App.cs
@@ -8,10 +8,12 @@ public class App
     const string ValetContainerRegistry = "ghcr.io";
 
     private readonly IDockerService _dockerService;
-
-    public App(IDockerService dockerService)
+    private readonly IProcessService _processService;
+    
+    public App(IDockerService dockerService, IProcessService processService)
     {
         _dockerService = dockerService;
+        _processService = processService;
     }
 
     public async Task<int> UpdateValetAsync(string? username = null, string? password = null, bool passwordStdin = false)
@@ -48,6 +50,24 @@ public class App
             "latest",
             args
         );
+        return 0;
+    }
+
+    public async Task<int> GetVersionAsync()
+    {
+        var ghVersion = await _processService.RunAndCaptureAsync("gh", "version");
+        var ghValetVersion = await _processService.RunAndCaptureAsync("gh", "extension list");
+        var valetVersion = await _processService.RunAndCaptureAsync("docker", $"run --rm {ValetContainerRegistry}/{ValetImage}:latest version", throwOnError:false);
+
+        var formattedGhVersion = ghVersion.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries).FirstOrDefault();
+        var formattedGhValetVersion = ghValetVersion.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries)
+            .FirstOrDefault(x => x.Contains("github/gh-valet"));
+        var formattedValetVersion = valetVersion.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries).FirstOrDefault() ?? "unknown";
+
+        Console.WriteLine(formattedGhVersion);
+        Console.WriteLine(formattedGhValetVersion);
+        Console.WriteLine($"valet-cli\t{formattedValetVersion}");
+
         return 0;
     }
 }
